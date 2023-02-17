dcdc4e8f1128b1161e937a05a34d6ca237c93335
Aisyah Nabillah <aisyahnabillah97@gmail.com>
2023-02-17 22:47:04 +0700

    Add file upload functionality and JSON response for uploaded file info

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Set up multer storage
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 
+// Handle file upload
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' })
+  }
 
+  const { originalname, mimetype, size } = req.file
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
